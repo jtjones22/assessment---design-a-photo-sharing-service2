00713b857a2b442c1aed32d78732f8865f484d73
Jonathan Jones <jonathan.jones51@yahoo.com>
2019-12-19 01:06:45 -0500

    Added getImages and postImages

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,15 +1,13 @@
 export const getImages = (request, response) => {
-    response.send({
-        "imageURIs": [
-          "Ello"
-        ],
-        "statusCode": 200
-      })
-}
+  response.send({
+    imageURIs: ["Ello"],
+    statusCode: 200
+  });
+};
 
 export const postImages = (request, response) => {
-    response.send({
-        "imageURI": "Testing",
-        "statusCode": 200
-      })
-}
+  response.send({
+    imageURI: "Testing",
+    statusCode: 200
+  });
+};
